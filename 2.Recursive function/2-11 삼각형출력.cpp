df9eb0fd861fc398ad84f1a6822f4058d3a3c7c1
y0on2q <ltnscp9028@gmail.com>
2020-07-21 22:13:58 +0900

    2-11 삼각형출력

---
+++ 2.Recursive function/2-11 삼각형출력.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+void g(int k){
+  if(k <= 0) return;
+  g(k-1);
+  printf("*");
+}
+
+void f(int k){
+  if(k <= 0) return;
+  f(k-1);
+  g(k);
+  puts("");
+}
+
+int main(){
+  cin>>n;
+  f(n);
+}
