1869cab130bdad1283b5e402471cf537ad547a27
y0on2q <ltnscp9028@gmail.com>
2020-07-24 16:31:02 +0900

    3-3 연속합

---
+++ 3. 수학 관련/3-3 연속합.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[100001],b[100001],n,r=-1000000000,i;
+main(){
+	for(cin>>n;i<n;cin >>a[i++]);
+	b[0]=a[0];
+	for(i=1;i<n;r=max(r,b[i++]))b[i]=max(b[i-1]+a[i],a[i]);
+	r=max(b[0],r);
+	cout<<r;
+}
